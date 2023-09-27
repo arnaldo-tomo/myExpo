302c077e4a46dc9fceada3170a2ae5d9aa90d406
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-27 11:09:53 +0200

    Update index.js

--- src/about/index.js
+++ src/about/index.js
@@ -1,18 +1,27 @@
-import { Alert, Image, ScrollView, Text, View } from "react-native";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  View,
+  ActivityIndicator,
+} from "react-native";
 import { AboutStyle } from "./styles";
 import axios, { Axios } from "axios";
 import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import Ionicons from "@expo/vector-icons";
 export default function About({ route }) {
   const [posts, setPosts] = useState([]);
+  const [carregando, setCarregando] = useState(true);
 
   const { nome } = route.params;
 
   useEffect(() => {
     axios
-      .get(`http://192.168.237.54:8000/api/busca/${nome}`)
+      .get(`https://pensador.bytebraine.com/api/busca/${nome}`)
       .then(function (response) {
         setPosts(response.data.allFrases);
-        console.log(response.data.allFrases);
       })
 
       .catch(function (error) {
@@ -20,24 +29,13 @@ export default function About({ route }) {
       });
   }, []);
 
-  if (posts.length === 0) {
-    return (
-      <View style={AboutStyle.error}>
-        <Image
-          source={require("../../assets/not.jpg")}
-          style={AboutStyle.img}
-        />
-      </View>
-    );
-  } else {
-    return (
-      <ScrollView>
-        {posts.map((item, index) => (
-          <View key={index} style={AboutStyle.container}>
-            <Text style={AboutStyle.texto}>{item.nome}</Text>
-          </View>
-        ))}
-      </ScrollView>
-    );
-  }
+  return (
+    <ScrollView>
+      {posts.map((item, index) => (
+        <View key={index} style={AboutStyle.container}>
+          <Text style={AboutStyle.texto}>{item.nome}</Text>
+        </View>
+      ))}
+    </ScrollView>
+  );
 }
