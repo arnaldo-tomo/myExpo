989b01fef03ef1bbfd3797b134311a8c6b22b6d0
Arnaldo tomo <arnaldotomo@gmail.com>
2023-09-28 12:33:53 +0200

    Create tamanho.js

---
+++ src/themes/tamanho.js
@@ -0,0 +1,11 @@
+// sizes.js
+
+// Tamanhos padrão
+export const SPACING_SMALL = 8;
+export const SPACING_MEDIUM = 16;
+export const SPACING_LARGE = 24;
+
+// Tamanhos de fonte
+export const FONT_SIZE_SMALL = 14;
+export const FONT_SIZE_MEDIUM = 18;
+export const FONT_SIZE_LARGE = 24;
